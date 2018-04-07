5aa4f5befa3f7ca8d2cfd501948e57540330971b
kikifreena <14kobarbara@gmail.com>
2018-04-06 19:17:41 -0700

    modified:   controller/home.css 	modified:   controller/orderPage.php 	modified:   controller/welcome.html

--- controller/orderPage.php
+++ controller/orderPage.php
@@ -1,6 +1,28 @@
 <?php
 
+?>
+<?php
+    $a = a;
+?>
 
+<html>
+<head>
+	<style> /*style for header and footer*/
+		header, footer {
+			padding: 1em;
+    		color: white;
+    		
+    		clear: left;
+    		text-align: center;
+		}
+	</style>
+	<title> Start an order </title>
+	<link href="home.css" type="text/css" rel="stylesheet"/>
+</head>
 
+<body>
+    <h1>Order some coffee</h1>
+    <p>Text</p>
+</body>
 
- ?>
+</html>
