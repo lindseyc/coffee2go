2fb3b62b8e40e79a4318e15e6a77a3fdf8f47e68
kikifreena <14kobarbara@gmail.com>
2018-04-06 19:51:38 -0700

    modified:   controller/home.css 	modified:   controller/orderPage.php 	modified:   controller/welcome.html

--- controller/orderPage.php
+++ controller/orderPage.php
@@ -14,7 +14,10 @@
     		
     		clear: left;
     		text-align: center;
-		}
+        }
+        button {
+            box-shadow: 0 4px #999;
+        }
 	</style>
 	<title> Start an order </title>
 	<link href="home.css" type="text/css" rel="stylesheet"/>
@@ -22,7 +25,18 @@
 
 <body>
     <h1>Order some coffee</h1>
-    <p>Text</p>
+    <form id="order">
+        <fieldset>
+            <legend>Customer information</legend>
+            Name: <input type="text" placeholder="First and Last"></input>
+        </fieldset>
+        <fieldset>
+            <legend>Drink</legend>
+        </fieldset>
+        <p></p>
+        <button class="brown" type="submit" value="submit">Order!</button>
+    </form>
+    <p>Thank you for your order!</p>
 </body>
 
 </html>
