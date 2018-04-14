986152a97449685690603fa89d5af71c94232ff3
kikifreena <14kobarbara@gmail.com>
2018-04-13 21:11:26 -0700

    new employee view

--- view/orderPage.php
+++ view/orderPage.php
@@ -1,10 +1,3 @@
-<?php
-
-?>
-<?php
-    $a = a;
-?>
-
 <html>
 <head>
 	<style> /*style for header and footer*/
@@ -30,12 +23,15 @@
         <fieldset>
             <legend>Customer information</legend>
             Name: <input type="text" placeholder="First and Last"></input>
+            Email: <input type="email" placeholder="@scrippscollege.edu"></input>
             <br/>
             Phone number: <input type="phone" placeholder="(909) 555-5555"></input>
             Carrier: <input type="text" placeholder="to receive texts"></input>
         </fieldset>
         <fieldset>
-            <legend>Drink</legend>
+            <legend>Drink
+                
+            </legend>
         </fieldset>
         <p></p>
         <button class="brown" type="submit" value="submit">Order!</button>
---
+++ view/submittedOrders.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<style> /*style for header and footer*/
+		header, footer {
+			padding: 1em;
+    		color: white;
+    		
+    		clear: left;
+    		text-align: center;
+        }
+        button {
+            box-shadow: 0 4px #999;
+            padding:10px;
+        }
+	</style>
+	<title> Submitted orders </title>
+	<link href="main.css" type="text/css" rel="stylesheet"/>
+</head>
+
+<body>
+    <h1> Your orders </h1>
+    <p>View orders that have been submitted. </p>
+</body>
