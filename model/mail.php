845e2ed572af61ef2ef0c047c9ead06f5d897130
kikifreena <14kobarbara@gmail.com>
2018-04-25 21:13:27 -0700

    mail

--- model/mail.php
+++ model/mail.php
@@ -1,10 +1,22 @@
 <?php
-$email_to="address@gmail.com";
-$email_subject="It works";
-$email_message="Hello. I can send mail!";
-$headers = "From: Me\r\n".
-"Reply-To: address@gmail.com\r\n'" .
+$path = ('carriersandaddresses.php');
+require_once($path);
+
+$phone_number = $_POST['phone']; // This should be a  POST somehow
+$carrier = $_POST['carrier'];
+// $carrier = 'T-Mobile'; // only for hard-coding
+$carrier_url = CellCarriers::$carriers[$carrier]; // post needs fixing but yeah
+// echo $carrier_url; // $_POST['carrier'];
+
+$email_to= $phone_number . "@" . $carrier_url;
+// echo "\n" . $email_to;
+$pickup_time = $_POST['timeDrop'];
+$email_subject="Order confirmation";
+$email_message="Your order is confirmed. It will be ready for pickup \
+in " . $pickup_time . 'minutes.';
+$headers = "From: noreply@scrippscollege.edu\r\n".
+"Reply-To: noreply\r\n'" .
 "X-Mailer: PHP/" . phpversion();
 mail($email_to, $email_subject, $email_message, $headers);  
-echo "mail sent!"
+echo "\nText message sent!"
 ?>
