b382986e040deb95324c7b13502a736cd06b3bd9
kikifreena <14kobarbara@gmail.com>
2018-04-29 22:49:22 -0700

    implement validation

--- view/validate.js
+++ view/validate.js
@@ -1,108 +1,97 @@
-
-
-// 1. call the validateFn function for fieldElem
-	// 2. find the sibling element span
-	// 3. if result of function call is true, update span to "Okay"
-	// if result of function call is false, update span to = infoMessage 
-
+var usernameRequirements = "At least 3 alpha characters";
+var emailReq = "Formatted in ***@***.edu/gov/com/org";
+var phoneReq = "Numbers only, no spaces or punctuation.";
 
 var validateField = function(fieldElem, infoMessage, validateFn) {
-	console.log("in validateField");
-	var span = $(fieldElem).siblings()[0];
-	
-	if(validateFn($(fieldElem).val())){
-		span.innerHTML = "OK";
-		$(span).removeClass();
-		$(span).addClass("ok");
-		return true;
-	}
-	else{
-		span.innerHTML = infoMessage;
-		$(span).addClass("error");
-		return false;
-	}
-}
-
-
-
-//check each field elt
-
-//The name ﬁeld must contain only alphabetical 
-//characters (first and last name). 
-var validateName = function (elem){
-	var regex = /^[a-z ,.'-]+$/i;
-
-// return regex.test(elem);
-	return true;
+	var text = $(fieldElem).val();
+	console.log(text);
+    var status = validateFn(text);
+    if (status===true){
+        $(fieldElem).removeClass("info");
+        $(fieldElem).removeClass("error");
+        $(fieldElem).addClass("ok");
+        $(fieldElem).next("span").replaceWith("<span style='color:green;'>OK</span>");
+        return true;
+    }
+    else if (status===false){
+        $(fieldElem).removeClass("ok");
+        $(fieldElem).addClass("error");
+        $(fieldElem).next("span").replaceWith("<span style='color:red;'>Error</span>");
+        return false;
+    }
+    else { // empty case, clear everything
+        $(fieldElem).removeClass("ok");
+        $(fieldElem).removeClass("error");
+        $(fieldElem).removeClass("info");
+        $(fieldElem).next("span").replaceWith("<span></span>");
+        return false;
+    }
 };
 
-
-var validatePhone = function (elem) {
-	var regex = /^\(?([0-9]{3})\)?[ . ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-// return regex.test(elem);
-
-	return true;
-}
-
-// var validateCarrier = function (elem) {
-
-// 	// return regex.test(elem);
-
-// 	return true;
-// }
-
-var validateEmail = function (elem) {
-	var regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
-
-// return regex.test(elem);
-
-	return true;
-}
-
-
-
-function validateForm(form) {
-	console.log("in validate form");
-
-	// if(!(validateName($('#name').val()))){ 
-	// 	alert('enter valid name');
-
-	// 	 return false;
-	// }
-	// else if (!validatePhone($('#phone').val())) {
-	// 	alert("enter valid phone number");
-
-	// 	 return false;
-	// }
-	// else if (!validateEmail($('#email').val())) {
-	// 	alert("enter valid email");
-
-	// 	 return false;
-	// }
-	
-
-	return true;
-}
-
-
 $(document).ready(function() {
-	//Use validateField to validate form fields on the page.
+    $("#name").focus(function(){info(this, usernameRequirements);});
+    $("#name").keyup(function(){validateField(this, usernameRequirements, usernameFn);});
+    $("#name").blur(function(){validateField(this, usernameRequirements, usernameFn);});
 
-	console.log("calling ready function");
+    $("#email").focus(function(){info(this,emailReq);});
+    $("#email").keyup(function(){validateField(this,emailReq, email);});
+    $("#email").blur(function(){validateField(this,emailReq, email);});
 
-	$('#name').on("focus", function() {
-		validateField($('#name'), 'Error: please enter a valid name (a-z characters)', validateName);
-	});
-	$('#phone').on("focus", function() {
-		validateField($('#phone'), 'Error: please enter a valid number (xxx xxx xxxx)', validatePhone);
-	});
+    $("#phone").focus(function(){info(this,phoneReq);});
+    $("#phone").keyup(function(){validateField(this,phoneReq, phone);});
+    $("#phone").blur(function(){validateField(this,phoneReq, phone);});
 
-	$('#email').on("focus", function() {
-		validateField($('#email'), 'Error: please enter a valid email address', validateEmail);
-	});
+    $("#submit").click(function(){validateAll();});
+});
 
-	// $('#carrier').on("focusout", function() {
-	// 	validateField($('#carrier'), 'Error: please enter a valid carrier', validateCarrier);
-	// });
+var info = function infoAppend (fieldElem, infoMessage){
+    $(fieldElem).removeClass("ok");
+    $(fieldElem).removeClass("error");
+    $(fieldElem).addClass("info");
+    $(fieldElem).next("span").replaceWith("<span style='color: gray;'>" + infoMessage + "</span>");
+};
+var usernameFn = function usernameValidate(text){
+    if (text===""){ return "empty";}
+    if (text.match(/^[A-Za-z\s]{3,}$/)) {return true;}
+    else { return false;}
+};
 
- });
+var stateFn = function stateValidate(text){
+    if (text===""){ return "empty";}
+    if (text.match(/^[A-Z]{2}$/)) {return true;}
+    else { return false;}
+};
+
+var phone = function Phone(text){
+    if (text===""){ return "empty";}
+    if (text.match(/^[0-9]{3}[0-9]{3}[0-9]{4}$/)){return true;}
+    else { return false; }
+};
+
+var email = function Email(text){
+    if (text===""){ return "empty";}
+    if (text.match(/^[a-zA-Z0-9]*@[a-zA-Z]*\.(gov|com|edu|org)$/)){
+        return true;}
+    else { return false; }
+};
+
+var selected = function Selected(errorElem, type, minimum){
+    var selectionLength = $("input[type=" + type + "]:checked");
+    if (selectionLength.length >= minimum){
+        $(errorElem).hide();
+        return true;}
+    else {
+        $(errorElem).show();
+        return false;}
+};
+
+var validateAll = function Submit(){
+    //username = validateField(":text:first", usernameRequirements, name);
+    var username = validateField("#inputusername", usernameRequirements, usernameFn);
+    var emailStatus = validateField("#inputemail", emailReq, email);
+    var phoneStatus = validateField("#inputphone", phoneReq, phone);
+    if (!(username && addressStatus && cityStatus && stateStatus && zipStatus && emailStatus && phoneStatus && troopStatus && gsStatus)){
+        $("#submiterror").show();
+        event.preventDefault();
+    }
+};
