14e3eb22161af71f9c5b7a2c00306825f17053ca
kikifreena <14kobarbara@gmail.com>
2018-04-29 23:05:41 -0700

    css/footer

--- view/confirmation.php
+++ view/confirmation.php
@@ -2,9 +2,34 @@
 <html>
 <head>
 	<title>Thank you</title>
+	<style> /*style for header and footer*/
+		header, footer {
+			padding: 1em;
+
+    		clear: left;
+    		text-align: center;
+        }
+        button {
+            box-shadow: 0 4px #999;
+            padding:10px;
+        }
+        .motto {
+            font-family: "Palatino", "Garamond", "Times", serif
+        }
+	</style>
+	<link href="./view/main.css" type="text/css" rel="stylesheet"/>
+
 </head>
 <body>
 	<h1>Thank you for your order!</h1>
 
+
+    <footer>
+        <hr/>
+        <p class='motto' style="font-weight:bold;">The Motley Coffeehouse</p>
+        <p class="motto"> - an intersectional, political, and feminist coffeehouse run by the students of Scripps College - </p>
+        <p>345 E. 9th Street <br/> Scripps College <br/> Claremont, CA 91711</p>
+        <p>(909) 607-3967 </p>
+    </footer>
 </body>
 </html>
